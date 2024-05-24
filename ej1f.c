e39755718473f26fb4db90b9172f52379df40439
Felipeig2 <turipanes234@gmail.com>
2024-05-24 10:41:56 -0300

    otro commit

--- ej1f.c
+++ ej1f.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(int argc, char *argv[]){
-    int a = atoi(argv[1]);
+    char mediopago = argv[1][0];
+    int prec = atoi(argv[2]);
+    int cant = atoi(argv[3]);
+    double total = prec * cant;
 
-    printf("El valor absoluto es %d", num);
+    if (mediopago == 'e') {
+      printf("%.2f\n", total * 0.85);
+    }
+    else if (mediopago == 't'){
+      printf("%.2f\n", total * 0.90);
+    }
+    else if (mediopago == 'c'){
+    printf("%.2f\n", total * 0.95);
+    }
+    else if (mediopago == 'd'){
+    printf("%.2f\n", total * 1.0);
+    }
+
+
+
+    // printf("Compraste un total de %d, y te sale %d", cant, prec);
     return 0;
 }

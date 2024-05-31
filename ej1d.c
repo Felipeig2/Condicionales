7b85a8f7838f706703d8804903a4fdc0bf927264
Felipeig2 <turipanes234@gmail.com>
2024-05-31 08:57:53 -0300

    Estos son ejercicios :D

--- ej1d.c
+++ ej1d.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <math.h>
 int main(int argc, char *argv[]){
 
-    int num = atoi(argv[1]);
-    if (num == 0){
-        printf("El numero es 0");
+    int a = atoi(argv[1]);
+    int b = atoi(argv[2]);
+    int c = atoi(argv[3]);
+    double D = ((b*b)-(4*a*c));
+    double x1 = (-b + sqrt(D)) / (2*a);
+    double x2 = (-b - sqrt(D)) / (2*a);
+    if (D == 0){
+        printf("La raices son iguales. valen %f", x1);
     }
-    else if (num > 0){
-        printf("El numero es positivo");
+    else if (D > 0){
+        printf("Tiene dos raices las cuales son %f, %f", x1, x2);
     }
     else{
-        printf("El numero es negativo");
+        printf("No tiene solucion");
     }
-
     return 0;
 }

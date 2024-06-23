84b7f75308a5ab51b0827ad86b172d9b186acb72
Felipeig2 <turipanes234@gmail.com>
2024-06-22 21:23:40 -0300

    agrego bien el 31

--- ej31.c
+++ ej31.c
@@ -1,22 +1,9 @@
+//1. Escribir un programa que realice la factorización de enteros en factores primos. Ejemplos:
 #include<stdio.h>
 #include<stdlib.h>
-#include <stdbool.h>
+#include<stdbool.h>
 #include<math.h>
 
-
-bool es_primo(int i);
-
-int main(int argc, char* argv[]){
-int n = atoi(argv[1]);
-
-for(int i = 2; i <= n; i++)
-
- if (es_primo(i)) {
-        n %= i;
-            printf("%d ", i);
-        }
-
-}
 bool es_primo(int n){
     for (int i = 2; i < n; i++){
         if (n % i == 0) return false;
@@ -24,6 +11,25 @@ bool es_primo(int n){
      return true;
 }
 
+int main(int argc, char* argv[]){
+int n = atoi(argv[1]);
+int ca = 0;
+for(int i = 2; i <= n; i++) {
+ 
+ while (es_primo(i) && n % i == 0) {
+        ca = n / i;
+        n -= ca;
+            printf("%d ", i);
+        }
+    }
+}
+
+/*  if (es_primo(i)) {
+        ca = n / i;
+        n -= ca;
+            printf("%d ", i);
+        }
+    }*/
 /*
  if( I % 2 == 0){
 i += 1;
